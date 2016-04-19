9086fde27eefb4ef6e637a7071b2e6e53020304a
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-19 07:34:50 -0500

    Fix relationship for owner

--- common/models/review.js
+++ common/models/review.js
@@ -2,8 +2,11 @@ module.exports = function(Review) {
   Review.beforeRemote('create', function(context, user, next) {
     var req = context.req;
 
-    req.body.date = Date.now();
-    req.body.publisherId = req.accessToken.userId;
+    req.body.data.attributes.date = Date.now();
+    req.body.data.relationships.reviewer.data = {
+      id: req.accessToken.userId,
+    };
+
     next();
   });
 };
